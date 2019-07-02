2b2990a4bd0a1fc6f1b37b94747724bf8f683c22
Kodziak <kodziak1416@gmail.com>
2019-07-02 23:42:04 +0200

    Add CORS handler.

--- main.go
+++ main.go
@@ -10,9 +10,18 @@ import (
 )
 
 func main() {
-fmt.Println("Connected.")
-
 	r := mux.NewRouter()
+	
+	// Handle all preflight request
+	r.Methods("OPTIONS").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// fmt.Printf("OPTIONS")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	})
+	
 	r.HandleFunc("/register", controller.RegisterHandler).
 		Methods("POST")
 	r.HandleFunc("/login", controller.LoginHandler).
@@ -20,5 +29,6 @@ fmt.Println("Connected.")
 	r.HandleFunc("/profile", controller.ProfileHandler).
 		Methods("GET")
 
+	fmt.Println("Server is ready and is listening at port :8000 . . .")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
