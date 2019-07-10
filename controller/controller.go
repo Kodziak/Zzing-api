0861817f6980835c9cb346dafbae1ed289b27899
Kodziak <kodziak1416@gmail.com>
2019-07-10 17:39:32 +0200

    Implement savings return function.

--- controller/controller.go
+++ controller/controller.go
@@ -164,7 +164,6 @@ func ProfileHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func SavingHandler(w http.ResponseWriter, r *http.Request) {
-	
 	w.Header().Set("Content-Type", "application/json")
 	user := model.User{}
 	body, _ := ioutil.ReadAll(r.Body)
@@ -211,31 +210,36 @@ func SavingHandler(w http.ResponseWriter, r *http.Request) {
 
 func GetSavingsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var user model.User
-	body, _ := ioutil.ReadAll(r.Body)
-	err := json.Unmarshal(body, &user)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	collection, err := db.GetDBCollection()
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	tokenString := r.Header.Get("Authorization")
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method")
+		}
+		return []byte("secret"), nil
+	})
 	var result model.User
 	var res model.ResponseResult
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		result.Username = claims["username"].(string)
+		result.Email = ""
+		result.Password = ""
 
-	err = collection.FindOne(context.TODO(), bson.D{{"username", user.Username}}).Decode(&result)
+		collection, err := db.GetDBCollection()
 
-	if err != nil {
-		res.Error = "Invalid username"
+		if err != nil {
+			res.Error = err.Error()
+			json.NewEncoder(w).Encode(res)
+			return
+		}
+
+		err = collection.FindOne(context.TODO(), bson.D{{"username", result.Username}}).Decode(&result)
+	
+		json.NewEncoder(w).Encode(result)
+		return
+	} else {
+		res.Error = err.Error()
 		json.NewEncoder(w).Encode(res)
 		return
-	}
-
-	result.Token = ""
-	result.Password = ""
-
-	json.NewEncoder(w).Encode(result)
+	}	
 }
